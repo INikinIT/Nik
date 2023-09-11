00378caf53b09e87557ee8dd41aeec7157b3c3aa
Nikolai_S <nikolaytestingit@gmail.com>
2023-09-11 12:32:58 +0300

    git_komit3

---
+++ dop_home/fail_1.py
@@ -0,0 +1,30 @@
+class Car:
+    def __init__(self, color, type, year):
+        self.color = color
+        self.type = type
+        self.year = year
+
+    def start(self):
+        print('Автомобиль заведен')
+
+    def stop(self):
+        print("Автомобиль заглушен")
+
+    def year_car(self):
+        print('Год производства автомобиля -', self.year)
+
+    def type_car(self):
+        print('Тип автомобиля -', self.type)
+
+    def color_car(self):
+        print('Цвет автомобиля -', self.color)
+
+
+car_1 = Car('Красный', "Спортивный", 2000)
+car_1.start()
+car_1.stop()
+car_1.year_car()
+car_1.type_car()
+car_1.color_car()
+
+
